f76e8e64840cf13534bcbdbbb07f5c8f5744577f
Anastasia <bezb.anastasia@gmail.com>
2019-05-07 19:57:59 +0300

    test is correct

--- app/test/test_which_profession.py
+++ app/test/test_which_profession.py
@@ -2,6 +2,6 @@ from app.lib import which_profession
 
 
 def test_which():
-    expected = "ISTJ: "
+    expected = "ISTG: "
     actual = which_profession('I', 'S', 'T', 'J')
     assert expected == actual
